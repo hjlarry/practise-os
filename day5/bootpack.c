c4ec83c4d346fe312f47c115136f13c595674f73
hjlarry <hjlarry@163.com>
2019-08-05 14:30:30 +0800

    添加字符集

--- day5/bootpack.c
+++ day5/bootpack.c
@@ -10,6 +10,7 @@ void set_palette(int start, int end, unsigned char *rgb);
 void boxfill8(unsigned char *vram, int xsize, unsigned char c, int x0, int y0, int x1, int y1);
 void init_screen(char *vram, int x, int y);
 void putfont8(char *vram, int xsize, int x, int y, char c, char *font);
+void putfonts8_asc(char *vram, int xsize, int x, int y, char c, unsigned char *s);
 
 #define COL8_000000 0
 #define COL8_FF0000 1
@@ -38,14 +39,13 @@ struct BOOTINFO
 void HariMain(void)
 {
     struct BOOTINFO *binfo = (struct BOOTINFO *) 0x0ff0;
-    static char font_A[16] = {
-		0x00, 0x18, 0x18, 0x18, 0x18, 0x24, 0x24, 0x24,
-		0x24, 0x7e, 0x42, 0x42, 0x42, 0xe7, 0x00, 0x00
-	};
+    extern char hankaku[4096];
 
     init_palette();
     init_screen(binfo->vram, binfo->scrnx, binfo->scrny);
-    putfont8(binfo->vram, binfo->scrnx, 10, 10, COL8_FFFF00, font_A);
+    putfonts8_asc(binfo->vram, binfo->scrnx, 10, 10, COL8_FFFF00, "ABC 123");
+    putfonts8_asc(binfo->vram, binfo->scrnx, 20, 30, COL8_FFFF00, "hello world");
+    putfonts8_asc(binfo->vram, binfo->scrnx, 30, 50, COL8_FFFF00, "test ~!@");
 
     for (;;)
     {
@@ -144,6 +144,16 @@ void putfont8(char *vram, int xsize, int x, int y, char c, char *font)
 		if ((d & 0x04) != 0) { p[5] = c; }
 		if ((d & 0x02) != 0) { p[6] = c; }
 		if ((d & 0x01) != 0) { p[7] = c; }
+	}
+	return;
+}
+
+void putfonts8_asc(char *vram, int xsize, int x, int y, char c, unsigned char *s)
+{
+	extern char hankaku[4096];
+	for (; *s != 0x00; s++) {
+		putfont8(vram, xsize, x, y, c, hankaku + *s * 16);
+		x += 8;
 	}
 	return;
 }
