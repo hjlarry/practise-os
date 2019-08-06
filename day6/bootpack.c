6b6adfe75fe1653d0b0768a4de32d7fce048660c
hjlarry <hjlarry@163.com>
2019-08-06 14:23:22 +0800

    添加中断处理程序

--- day6/bootpack.c
+++ day6/bootpack.c
@@ -8,6 +8,9 @@ void HariMain(void)
     int mx, my;
 
     init_gdtidt();
+    init_pic();
+    io_sti(); /* IDT/PIC的初始化已经完成，于是开放CPU的中断 */
+
     init_palette();
     init_screen(binfo->vram, binfo->scrnx, binfo->scrny);
     putfonts8_asc(binfo->vram, binfo->scrnx, 10, 10, COL8_FFFF00, "ABC 123");
@@ -21,6 +24,9 @@ void HariMain(void)
     my = (binfo->scrny - 28 - 16) / 2;
     init_mouse_cursor8(mcursor, COL8_008484);
     putblock8_8(binfo->vram, binfo->scrnx, 16, 16, mx, my, mcursor, 16);
+
+    io_out8(PIC0_IMR, 0xf9); /* 开放PIC1和键盘中断(11111001) */
+	io_out8(PIC1_IMR, 0xef); /* 开放鼠标中断(11101111) */
 
     for (;;)
     {
--- day6/dsctbl.c
+++ day6/dsctbl.c
@@ -1,3 +1,5 @@
+/* GDT、IDT、descriptor table 关系处理 */
+
 #include "bootpack.h"
 
 void init_gdtidt(void)
@@ -23,6 +25,11 @@ void init_gdtidt(void)
         set_gatedesc(idt + i, 0, 0, 0);
     }
     load_idtr(LIMIT_IDT, ADR_IDT);
+
+    /* IDT设置*/
+	set_gatedesc(idt + 0x21, (int) asm_inthandler21, 2 * 8, AR_INTGATE32);
+	set_gatedesc(idt + 0x27, (int) asm_inthandler27, 2 * 8, AR_INTGATE32);
+	set_gatedesc(idt + 0x2c, (int) asm_inthandler2c, 2 * 8, AR_INTGATE32);
 
     return;
 }
