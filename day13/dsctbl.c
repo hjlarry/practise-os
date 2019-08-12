cc93e111b0ecf72a708a5d15f169f0405c8b1e1e
hjlarry <hjlarry@163.com>
2019-08-12 16:51:35 +0800

    抽象字符串显示方法和定时器缓冲区

---
+++ day13/dsctbl.c
@@ -0,0 +1,62 @@
+/* GDT、IDT、descriptor table 关系处理 */
+
+#include "bootpack.h"
+
+void init_gdtidt(void)
+{
+    struct SEGMENT_DESCRIPTOR *gdt = (struct SEGMENT_DESCRIPTOR *)ADR_GDT;
+    struct GATE_DESCRIPTOR *idt = (struct GATE_DESCRIPTOR *)ADR_IDT;
+    int i;
+
+    /* GDT(全局段号记录表)初始化,将8192个段的上限，基址，访问权限都设为0 */
+    for (i = 0; i <= LIMIT_GDT / 8; i++)
+    {
+        set_segmdesc(gdt + i, 0, 0, 0);
+    }
+    /* 段号为1的段，表示CPU所能管理的全部内存，上限为4G */
+    set_segmdesc(gdt + 1, 0xffffffff, 0x00000000, AR_DATA32_RW);
+    /* 段号为2的段，为bootpack.hrb准备，大小512KB，从0x00280000开始 */
+    set_segmdesc(gdt + 2, LIMIT_BOTPAK, ADR_BOTPAK, AR_CODE32_ER);
+    load_gdtr(LIMIT_GDT, ADR_GDT);
+
+    /* IDT（中断记录表）初始化 */
+    for (i = 0; i < LIMIT_IDT / 8; i++)
+    {
+        set_gatedesc(idt + i, 0, 0, 0);
+    }
+    load_idtr(LIMIT_IDT, ADR_IDT);
+
+    /* IDT设置*/
+	set_gatedesc(idt + 0x20, (int) asm_inthandler20, 2 * 8, AR_INTGATE32);
+	set_gatedesc(idt + 0x21, (int) asm_inthandler21, 2 * 8, AR_INTGATE32);
+	set_gatedesc(idt + 0x27, (int) asm_inthandler27, 2 * 8, AR_INTGATE32);
+	set_gatedesc(idt + 0x2c, (int) asm_inthandler2c, 2 * 8, AR_INTGATE32);
+
+    return;
+}
+
+void set_segmdesc(struct SEGMENT_DESCRIPTOR *sd, unsigned int limit, int base, int ar)
+{
+    if (limit > 0xfffff)
+    {
+        ar |= 0x8000; /* G_bit = 1 */
+        limit /= 0x1000;
+    }
+    sd->limit_low = limit & 0xffff;
+    sd->base_low = base & 0xffff;
+    sd->base_mid = (base >> 16) & 0xff;
+    sd->access_right = ar & 0xff;
+    sd->limit_high = ((limit >> 16) & 0x0f) | ((ar >> 8) & 0xf0);
+    sd->base_high = (base >> 24) & 0xff;
+    return;
+}
+
+void set_gatedesc(struct GATE_DESCRIPTOR *gd, int offset, int selector, int ar)
+{
+    gd->offset_low = offset & 0xffff;
+    gd->selector = selector;
+    gd->dw_count = (ar >> 8) & 0xff;
+    gd->access_right = ar & 0xff;
+    gd->offset_high = (offset >> 16) & 0xffff;
+    return;
+}
