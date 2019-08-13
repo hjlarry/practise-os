829d7e6725c4935f04d50299b1320b720b294a0e
hjlarry <hjlarry@163.com>
2019-08-13 14:40:38 +0800

    提高分辨率

---
+++ day14/mouse.c
@@ -0,0 +1,86 @@
+/* 鼠标控制代码 */
+
+#include "bootpack.h"
+
+struct FIFO32 *mousefifo;
+int mousedata0;
+
+void inthandler2c(int *esp)
+/* 来自PS/2鼠标的中断 */
+{
+    int data;
+    io_out8(PIC1_OCW2, 0x64); /* 通知PIC IRQ-12 已经受理完毕 */
+    io_out8(PIC0_OCW2, 0x62); /* 通知PIC IRQ-02 已经受理完毕 */
+    data = io_in8(PORT_KEYDAT);
+    fifo32_put(mousefifo, data + mousedata0);
+    return;
+}
+
+#define KEYCMD_SENDTO_MOUSE 0xd4
+#define MOUSECMD_ENABLE 0xf4
+
+void enable_mouse(struct FIFO32 *fifo, int data0, struct MOUSE_DEC *mdec)
+{
+    mousefifo = fifo;
+	mousedata0 = data0;
+    /* 激活鼠标 */
+    wait_KBC_sendready();
+    io_out8(PORT_KEYCMD, KEYCMD_SENDTO_MOUSE);
+    wait_KBC_sendready();
+    io_out8(PORT_KEYDAT, MOUSECMD_ENABLE);
+    /* 顺利的话，ACK(0xfa)会被送过来 */
+    mdec->phase = 0; /* 等待0xfa阶段 */
+    return;
+}
+
+int mouse_decode(struct MOUSE_DEC *mdec, unsigned char dat)
+{
+    if (mdec->phase == 0)
+    {
+        /* 等待鼠标的0xfa阶段 */
+        if (dat == 0xfa)
+        {
+            mdec->phase = 1;
+        }
+        return 0;
+    }
+    if (mdec->phase == 1)
+    {
+        /* 等待鼠标的第一字节的阶段 */
+        if ((dat & 0xc8) == 0x08)
+        /* 判断第一个字节是正确的 */
+        {
+            mdec->buf[0] = dat;
+            mdec->phase = 2;
+        }
+
+        return 0;
+    }
+    if (mdec->phase == 2)
+    {
+        /* 等待鼠标的第二字节的阶段 */
+        mdec->buf[1] = dat;
+        mdec->phase = 3;
+        return 0;
+    }
+    if (mdec->phase == 3)
+    {
+        /* 等待鼠标的第三字节的阶段 */
+        mdec->buf[2] = dat;
+        mdec->phase = 1;
+        mdec->btn = mdec->buf[0] & 0x07;
+        mdec->x = mdec->buf[1];
+        mdec->y = mdec->buf[2];
+        if ((mdec->buf[0] & 0x10) != 0)
+        {
+            mdec->x |= 0xffffff00;
+        }
+        if ((mdec->buf[0] & 0x20) != 0)
+        {
+            mdec->y |= 0xffffff00;
+        }
+        mdec->y = -mdec->y; /* 鼠标的y方向与画面符号相反 */
+        return 1;
+    }
+    return -1; /* 应该不会走到这里 */
+}
